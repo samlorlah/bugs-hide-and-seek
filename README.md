c0fd2783a3ee00a792b4085066b169a8f832dfd4
Muyiwa Fawole <fawolemuyiwa@rocketmail.com>
2022-07-22 20:39:26 +0100

    Project Commit

---
+++ README.md
@@ -0,0 +1,43 @@
+![screenshot](https://user-images.githubusercontent.com/48407831/178073604-fdb2e129-cf45-4076-871b-39912f651428.PNG)
+
+# Project: My Portfolio
+
+# Getting Started
+
+> Welcome to my `Portfolio` Repo. This is where the main journey at Microverse begins. This projects gives you the opportunity to see how my professional career as a programmer starts.
+
+# Website (Online Version)
+>You can view the online version of this webisite using this [link](https://samlorlah.github.io/portfolio/).
+## Built With
+
+- Major languages - HTML /CSS
+- Frameworks - N/A
+- Technologies used - N / A
+
+## Authors
+
+👤 **Oluwamuyiwa Fawole**
+
+- GitHub: [@samlorlah](https://github.com/samlorlah)
+- Twitter: [@samlorlah](https://twitter.com/samlorlah)
+- LinkedIn: [LinkedIn](https://www.linkedin.com/in/muyiwa-fawole/)
+
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the [issues page](../../issues/).
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## Acknowledgments
+
+- Hat tip to anyone whose code was used
+- Inspiration
+- etc
+
+## 📝 License
+
+This project is [MIT](./MIT.md) licensed.
---
+++ asset/js/script.js
@@ -0,0 +1,56 @@
+const hamburger = document.getElementById('hamburger');
+const closeMenu = document.getElementById('close-menu-icon');
+const mobileMenu = document.getElementById('mobile-menu');
+const menuList = document.querySelector('.menu-list');
+const modalClose = document.querySelectorAll('.modal-close');
+const modalDialog = document.querySelectorAll('.popup');
+const modals = document.querySelectorAll('.action-btn');
+const contactForm = document.getElementById('contact-me-form');
+
+hamburger.addEventListener('click', () => {
+  mobileMenu.style.display = 'block';
+});
+
+const hideMenu = () => {
+  mobileMenu.style.display = "none";
+};
+
+closeMenu.addEventListener('click', hideMenu());
+
+menuList.addEventListener('click', hideMenu);
+
+for (let i = 0; i < modalClose.length; i += 1) {
+  modalClose[i].addEventListener('click', () => {
+    for (let j = 0; j < modalDialog.length; j += 1) {
+      if (modalDialog[j].style.display === 'block') {
+        modalDialog[j].style.display = 'none';
+      }
+    }
+  });
+}
+
+for (let i = 0; i < modals.length; i += 1) {
+  modals[i].addEventListener('click', () => {
+    const modal = document.querySelector(modals[i].getAttribute('data-target'));
+    modal.style.display = 'block';
+  });
+}
+
+const validateEmail = (email) => {
+  if (email !== email.toLowerCase()) return false;
+  return true;
+};
+
+contactForm.addEventListener('submit', (e) => {
+  const EMAIL_INVALID = 'Your Email should be in lower case only. Please try again!';
+  const displayMsg = document.getElementById('display-msg');
+  e.preventDefault();
+  if (!validateEmail(contactForm.elements.email.value.trim())) {
+    displayMsg.innerText = EMAIL_INVALID;
+    displayMsg.style.color = '#ff0000';
+  } else {
+    displayMsg.innerText = '';
+    localStorage.removeItem('form-data');
+    contactForm.submit();
+  }
+});
